234acd246f6dd754aea2ee407d7eb4d48d2b7bff
MasDepan <50104357+adwerygaming@users.noreply.github.com>
2026-02-12 19:32:55 +0700

    chore(sholatku): Check prayer times for all locations
    - chore(sholatku): Import tags utility for logging
    - chore(sholatku): Iterate through all retrieved locations
    - chore(sholatku): Call checkPrayer for each location
    - chore(sholatku): Add console logs for progress and results

--- src/sholatku/Index.ts
+++ src/sholatku/Index.ts
@@ -1,6 +1,7 @@
 import moment from "moment-timezone"
 import path from "path"
 import { _dirname } from "../utils/Path.js"
+import tags from "../utils/Tags.js"
 import SholatKuService from "./SholatKuService.js"
 
 const dataPath = path.join(_dirname, "..", "assets", "schedule.json")
@@ -17,4 +18,14 @@ const city = "Kota Jakarta"
 // const reg = await SholatKuService.User(id).register(province, city)
 // console.log(reg)
 
-await SholatKuService.getAllLocations()
+const locations = await SholatKuService.getAllLocations()
+
+for (let i = 0; i < locations.length; i++) {
+    const location = locations[i];
+    
+    console.log(`[${tags.System}] Checking prayer time for ${location.province} - ${location.city}`)
+    const check = await SholatKuService.checkPrayer({ province: location.province, city: location.city, debugTime: now })
+    console.log(check)
+
+    console.log("")
+}
