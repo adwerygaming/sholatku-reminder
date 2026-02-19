2be626d1b89e68c79064ce16e5a25da536ce7e2d
MasDepan <50104357+adwerygaming@users.noreply.github.com>
2026-02-19 14:44:21 +0700

    docs(UserAccount): Enhance JSDoc (nyicil lagi)
    - docs(UserAccount): Add JSDoc for getPrayerState method
    - docs(UserAccount): Add JSDoc for setPrayerState method
    - docs(UserAccount): Add JSDoc for getProvince method
    - docs(UserAccount): Add JSDoc for setProvince method
    - docs(UserAccount): Add JSDoc for getCity method
    - docs(UserAccount): Add JSDoc for setCity method
    - docs(UserAccount): Add JSDoc for unregister method
    - docs(UserAccount): Add JSDoc for fetch method
    - docs(UserAccount): Add JSDoc for isRegistered method

--- src/sholatku/domain/UserAccount.ts
+++ src/sholatku/domain/UserAccount.ts
@@ -11,6 +11,11 @@ export class UserAccount {
         private readonly prayerStateDb = DatabaseClient.table("user_prayer_state")
     ) { }
 
+    /**
+     * ### Get this user's prayer state of this eventName
+     * @param eventName 
+     * @returns boolean value of that user's prayer state
+     */
     async getPrayerState(eventName: string): Promise<boolean> {
         const now = moment()
         const dayIdentifier = now.format("DD_MM")
@@ -20,6 +25,11 @@ export class UserAccount {
         return res ? true : false
     }
 
+    /**
+     * ### Sets this user's prayer state of this eventName
+     * @param eventName 
+     * @param value boolean value for that user's prayer state
+     */
     async setPrayerState(eventName: string, value: boolean): Promise<void> {
         const now = moment()
         const dayIdentifier = now.format("DD_MM")
@@ -28,11 +38,19 @@ export class UserAccount {
 
     // =======
 
+    /**
+     * ### Get this user's province from database
+     * @returns Province value of that user. Could be string or null.
+     */
     async getProvince(): Promise<string | null> {
         const res = await this.db.get<string>(`${this.user.id}.location.province`)
         return res || null
     }
 
+    /**
+     * ### Set this user's province to database
+     * @param province Province value for that user
+     */
     async setProvince(province: string): Promise<void> {
         province = SholatKuServiceHelper.normalizeInput(province)
 
@@ -40,11 +58,19 @@ export class UserAccount {
         await this.db.set(`${this.user.id}.location.lastUpdatedAt`, moment().toISOString())
     }
 
+    /**
+     * ### Get this user's city from database
+     * @returns City value of hat user. Could be string or null.
+     */
     async getCity(): Promise<string | null> {
         const res = await this.db.get<string>(`${this.user.id}.location.city`)
         return res || null
     }
 
+    /**
+     * ### Set this user's city to database
+     * @param city City value for that user
+     */
     async setCity(city: string): Promise<void> {
         city = SholatKuServiceHelper.normalizeInput(city)
 
@@ -52,15 +78,31 @@ export class UserAccount {
         await this.db.set(`${this.user.id}.location.lastUpdatedAt`, moment().toISOString())
     }
 
+    /**
+     * ### Unregister this user by deleting location data from database. 
+     * ----
+     * This will make the user not receive any prayer event notification, 
+     * because the service doesn't know where this user is located.
+     */
     async unregister(): Promise<void> {
         await this.db.delete(`${this.user.id}.location`)
     }
 
+    /**
+     * ### Fetch this full user data (SholatkuUser) from database.
+     * @returns SholatkuUser object. Could be null if user not exist in database.
+     */
     async fetch(): Promise<SholatkuUser | null> {
         const user = await this.db.get<SholatkuUser>(`${this.user.id}`)
         return user
     }
 
+    /**
+     * ### Check if this user is registered or not
+     * ---
+     * By checking the existence of location data in database.
+     * @returns boolean value of that user is registered or nah.
+     */
     async isRegistered(): Promise<boolean> {
         const user = await this.db.get<SholatkuUser>(`${this.user.id}`)
         return user?.location ? true : false
