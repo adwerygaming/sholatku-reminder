cc74f7924149c6d26bb1951d3b5b757306a556d8
MasDepan <50104357+adwerygaming@users.noreply.github.com>
2026-02-20 14:21:07 +0700

    refactor(sholatku): Modularize client
    - refactor(sholatku): Extract event emitter and scheduling logic to Client.ts
    - refactor(sholatku): Move PrayerEventPayload type definition to Client.ts
    - refactor(sholatku): Update event listeners to use new sholatkuClient instance
    - chore(sholatku): Remove unused internal domain model imports

--- src/sholatku/Index.ts
+++ src/sholatku/Index.ts
@@ -1,51 +1,10 @@
-import moment, { Moment } from "moment-timezone";
-import EventEmitter from "node:events";
+import moment from "moment-timezone";
 import client from "../discord/Client.js";
 import { PrayerEvent } from "../types/Prayer.types.js";
-import { SholatkuUser, UserProvider } from "../types/Users.types.js";
+import { UserProvider } from "../types/Users.types.js";
 import tags from "../utils/Tags.js";
-import { Location } from "./domain/Location.js";
-import { CycleCheckEvent, PrayerScheduler } from "./domain/PrayerScheduler.js";
-import { UserAccount } from "./domain/UserAccount.js";
-import { UserManager } from "./domain/UserManager.js";
+import { PrayerEventPayload, sholatkuClient } from "./Client.js";
 import SholatKuServiceHelper from "./helper/Helper.js";
-
-type PrayerEventPayload = {
-    event: CycleCheckEvent;
-    province: {
-        searchKey: string;
-        original: string;
-        databaseKey: string;
-    };
-    city: {
-        searchKey: string;
-        original: string;
-        databaseKey: string;
-    };
-    users: SholatkuUser[];
-};
-
-type EventMap = {
-  [K in PrayerEvent]: PrayerEventPayload;
-};
-
-export class SholatKuEmitter extends EventEmitter {
-  emit<K extends keyof EventMap>(
-    event: K,
-    payload: EventMap[K]
-  ): boolean {
-    return super.emit(event, payload);
-  }
-
-  on<K extends keyof EventMap>(
-    event: K,
-    listener: (payload: EventMap[K]) => void
-  ): this {
-    return super.on(event, listener);
-  }
-}
-
-export const sholatKuEmitter = new SholatKuEmitter();
 
 async function annouce(payload: PrayerEventPayload, message: string) {
     // const users: SholatkuUser[] = payload.users.map((u) => u)
@@ -83,7 +42,7 @@ async function annouce(payload: PrayerEventPayload, message: string) {
     }
 }
 
-sholatKuEmitter.on(PrayerEvent.PrayerTime, async (payload) => {
+sholatkuClient.on(PrayerEvent.PrayerTime, async (payload) => {
     console.log(`[${tags.PrayerService}] Prayer Time Event for ${SholatKuServiceHelper.capitalizeWords(payload.event.eventName)} in ${payload.city}, ${payload.province} for users: ${payload.users.map(u => u.id).join(", ")}`);
 
     let eventName = payload.event.eventName
@@ -104,7 +63,7 @@ sholatKuEmitter.on(PrayerEvent.PrayerTime, async (payload) => {
     }
 });
 
-sholatKuEmitter.on(PrayerEvent.PrayerIn5m, async (payload) => {
+sholatkuClient.on(PrayerEvent.PrayerIn5m, async (payload) => {
     console.log(`[${tags.PrayerService}] Prayer In 5 Minutes Event for ${SholatKuServiceHelper.capitalizeWords(payload.event.eventName)} in ${payload.city}, ${payload.province} for users: ${payload.users.map(u => u.id).join(", ")}`);
 
     let eventName = payload.event.eventName
@@ -123,7 +82,7 @@ sholatKuEmitter.on(PrayerEvent.PrayerIn5m, async (payload) => {
     }
 });
 
-sholatKuEmitter.on(PrayerEvent.PrayerIn15m, async (payload) => {
+sholatkuClient.on(PrayerEvent.PrayerIn15m, async (payload) => {
     console.log(`[${tags.PrayerService}] Prayer In 15 Minutes Event for ${SholatKuServiceHelper.capitalizeWords(payload.event.eventName)} in ${payload.city}, ${payload.province} for users: ${payload.users.map(u => u.id).join(", ")}`);
 
     let eventName = payload.event.eventName
@@ -142,7 +101,7 @@ sholatKuEmitter.on(PrayerEvent.PrayerIn15m, async (payload) => {
     }
 });
 
-sholatKuEmitter.on(PrayerEvent.PrayerIn30m, async (payload) => {
+sholatkuClient.on(PrayerEvent.PrayerIn30m, async (payload) => {
     console.log(`[${tags.PrayerService}] Prayer In 30 Minutes Event for ${SholatKuServiceHelper.capitalizeWords(payload.event.eventName)} in ${payload.city}, ${payload.province} for users: ${payload.users.map(u => u.id).join(", ")}`);
 
     let eventName = payload.event.eventName
@@ -158,110 +117,3 @@ sholatKuEmitter.on(PrayerEvent.PrayerIn30m, async (payload) => {
         annouce(payload, `**${SholatKuServiceHelper.capitalizeWords(payload.event.eventName)}** prayer will begin in 30 minutes at ${payload.event.time.format("HH:mm")}.`)
     }
 });
-
-// await DatabaseClient.table("users").deleteAll()
-// await DatabaseClient.table("prayer_state").deleteAll()
-// await DatabaseClient.table("user_prayer_state").deleteAll()
-
-// const provTest = "yogya"
-// const cityTest = "gunung"
-
-// const provRes = await SholatKuService.Database.Location.searchProvince(provTest)
-// console.log("Province Search Result:", provRes)
-// const cityRes = await SholatKuService.Database.Location.searchCity(provRes?.original ?? provTest, cityTest)
-// console.log("City Search Result:", cityRes)
-
-setInterval(async () => {
-    await check()
-}, 5000);
-
-await check()
-
-async function check() {
-    let debugTime: Moment | undefined = undefined
-    const useDebugTime = false
-
-    if (useDebugTime) {
-        debugTime = moment("11:50", "HH:mm")
-    }
-
-    const user = new UserManager()
-
-    const users = await user.getAll()
-    const usersMap = new Map<string, SholatkuUser[]>()
-
-    for (let i = 0; i < users.length; i++) {
-        const user = users[i];
-        const province = user.location?.province
-        const city = user.location?.city
-
-        if (!province || !city) continue;
-
-        const locationKey = `${province}-${city}`
-
-        if (!usersMap.has(locationKey)) {
-            usersMap.set(locationKey, [])
-        }
-
-        usersMap.get(locationKey)!.push(user)
-    }
-
-    const thing = [...usersMap.entries()]
-
-    for (let i = 0; i < thing.length; i++) {
-        const res = thing[i];
-        const locationKeyRaw = res[0]
-        const users = res[1]
-
-        const [province, city] = locationKeyRaw.split("-")
-
-        if (!province || !city) continue;
-
-        const prayerScheduler = new PrayerScheduler(province, city, debugTime)
-        const location = new Location()
-
-        const check = await prayerScheduler.cycleCheck()
-
-        if (!check) continue;
-
-        const provinceFinal = await location.searchProvince(province)
-        const cityFinal = await location.searchCity(provinceFinal.original, city)
-
-        for (let k = 0; k < check.length; k++) {
-            const event = check[k];
-
-            if (!event) continue;
-
-            await sholatKuEmitter.emit(event.type, {
-                event,
-                province: {
-                    databaseKey: province,
-                    original: provinceFinal.original,
-                    searchKey: provinceFinal.searchKey
-                },
-                city: {
-                    databaseKey: city,
-                    original: cityFinal.original,
-                    searchKey: cityFinal.searchKey
-                },
-                users
-            })
-            // leave to this thing to update the last state
-            for (let j = 0; j < users.length; j++) {
-                const user = users[j];
-                const userAccount = new UserAccount(user)
-
-                const key = `${event.type}-${event.eventName}`
-                const stateCheck = await userAccount.getPrayerState(key)
-
-                if (event.type == "prayerTime" && !stateCheck) {
-                    await userAccount.setPrayerState(key, true)
-                } else if (event.type == "prayer_in_15m" && !stateCheck) {
-                    await userAccount.setPrayerState(key, true)
-                } else if (event.type == "prayer_in_30m" && !stateCheck) {
-                    await userAccount.setPrayerState(key, true)
-                }
-            }
-        }
-    }
-}
