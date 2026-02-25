e19d0aa4469720f17eb8e5e3b03a3ccbf751ca5c
MasDepan <50104357+adwerygaming@users.noreply.github.com>
2026-02-25 11:53:54 +0700

    Update README with project overview and usage guide
    - docs(readme): add project title, description, and centering
    - docs(readme): include GitHub and Discord status badges
    - docs(readme): describe available prayer event types
    - docs(readme): provide TypeScript code example for event usage

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-<div>
-
+<div align="center">
+    <h1>Prayer Reminder</h1>
+    <p>A Islamic prayer reminder system using Node.js</p>
+    <img alt="GitHub commits since latest release" src="https://img.shields.io/github/commits-since/adwerygaming/sholatku-reminder/latest?style=for-the-badge">
+    <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/adwerygaming/sholatku-reminder?style=for-the-badge">
+    <img alt="Discord" src="https://img.shields.io/discord/561136271901196298?style=for-the-badge&label=Discord&logo=discord">
+    <img alt="GitHub issue custom search in repo" src="https://img.shields.io/github/issues-search/adwerygaming/sholatku-reminder?query=is%3Aopen&style=for-the-badge&label=Open%20Issues">
 </div>
+
+## Usage
+There are <b>4 event available to use</b>, that includes `PrayerTime`, `PrayerIn5m`, `PrayerIn15m`, and `PrayerIn30m`. Each event will trigger at the specified time before the prayer time, allowing you to set up reminders or perform any necessary actions.
+```ts
+import sholatkuClient from "./Client.js";
+
+sholatkuClient.on(PrayerEvent.PrayerTime, async (payload) => {
+    // Triggered when the it's time for prayer
+});
+
+sholatkuClient.on(PrayerEvent.PrayerIn5m, async (payload) => {
+    // Triggered 5 minutes before the prayer time
+});
+
+sholatkuClient.on(PrayerEvent.PrayerIn15m, async (payload) => {
+    // Triggered 15 minutes before the prayer time
+});
+
+sholatkuClient.on(PrayerEvent.PrayerIn30m, async (payload) => {
+    // Triggered 30 minutes before the prayer time
+});
+```
---
+++ src/discord/Listener.ts
@@ -0,0 +1,198 @@
+import moment from "moment-timezone";
+import sholatkuClient, { PrayerEventPayload } from "../sholatku/Client.js";
+import { PrayerEvent } from "../types/Prayer.types.js";
+import tags from "../utils/Tags.js";
+
+import { Colors, ContainerBuilder, MessageFlags } from "discord.js";
+import { capitalizeWords } from "../sholatku/helper/Helper.js";
+import client from "./Client.js";
+
+export class DiscordListener {
+    private readonly guildId: string | undefined
+    private readonly channelId: string | undefined
+
+    constructor (
+        guildId: string,
+        channelId: string
+    ) {
+        this.guildId = guildId
+        this.channelId = channelId
+        console.log(`[${tags.Discord}] Loaded Discord Listener Script.`)
+    }
+
+    private async sendMessage(payload: PrayerEventPayload, message: string): Promise<void> {
+        const isDiscordUser = payload.users.some(u => u.provider == "discord")
+        if (!isDiscordUser) return
+
+        if (!this.guildId) {
+            console.log(`[${tags.Discord}] Guild ID is not set. Cannot send message to Discord.`)
+            return
+        }
+
+        if (!this.channelId) {
+            console.log(`[${tags.Discord}] Channel ID is not set. Cannot send message to Discord.`)
+            return
+        }
+
+        const guilds = await client.guilds.fetch();
+        const guild = await guilds.get(this.guildId)?.fetch();
+
+        if (!guild) {
+            console.log(`[${tags.Discord}] Cannot find guild with ID ${this.guildId}`);
+            return
+        }
+
+        const channels = await guild.channels.fetch();
+        const channel = await channels.get(this.channelId)?.fetch();
+
+        if (!channel?.isTextBased()) {
+            console.log(`[${tags.Discord}] Provided channel is not a text channel. Cannot send message to Discord.`);
+            return
+        }
+
+        const footer = `-# ${capitalizeWords(payload.province.original)}, ${capitalizeWords(payload.city.original)} - ${moment().format("HH:mm:ss")}`
+
+        try {
+            const messageContainer = new ContainerBuilder()
+                .setAccentColor(Colors.Blurple)
+                .addTextDisplayComponents(
+                    text => text.setContent(message)
+                )
+                .addTextDisplayComponents(
+                    text => text.setContent(footer)
+                )
+
+            await channel.send({
+                components: [messageContainer],
+                flags: [MessageFlags.IsComponentsV2]
+            })
+        } catch (e) {
+            console.log(`[${tags.Error}] Failed to send message to Discord: ${e}`);
+        }
+    }
+
+    async listen(): Promise<void> {
+        console.log(`[${tags.Discord}] Started listening to Sholatku events.`)
+
+        // Prayer Time Event
+        sholatkuClient.on(PrayerEvent.PrayerTime, async (payload) => {
+            let eventName = payload.event.eventName
+            const dayName = moment().tz("Asia/Jakarta").locale("en").format("dddd")?.toLowerCase()
+            
+            let message: string | null = null
+
+            switch (eventName) {
+                case "imsak":
+                    message = `Time for **Imsyakiyah**.`
+                    break;
+
+                case "terbit": 
+                    message = `**The Sun has risen.**`
+                    break;
+
+                case "dhuha":
+                    message = `**Dhuha** prayer has started.`
+                    break;
+            
+                default:
+                    if (dayName == "friday" && eventName == "dzuhur") {
+                        eventName = "jummah"
+                    }
+
+                    message = `Time for **${capitalizeWords(eventName)}** prayer.`
+                    break;
+            }
+
+            await this.sendMessage(payload, message)
+        })
+
+        sholatkuClient.on(PrayerEvent.PrayerIn5m, async (payload) => {
+            let eventName = payload.event.eventName
+            const eventTime = payload.event.time.format("HH:mm")
+            const dayName = moment().tz("Asia/Jakarta").locale("en").format("dddd")?.toLowerCase()
+            
+            let message: string | null = null
+
+            switch (eventName) {
+                case "imsak":
+                    message = `**Imsyakiyah** will begin in **5 minutes** at ${eventTime}.`
+                    break;
+
+                case "terbit":
+                    message = `**The sun will rise in 5 minutes** at ${eventTime}.`
+                    break;
+
+                case "dhuha":
+                    message = `**Dhuha** prayer will start in 5 minutes at ${eventTime}.`
+                    break;
+
+                default:
+                    if (dayName == "friday" && eventName == "dzuhur") {
+                        eventName = "jummah"
+                    }
+
+                    message = `**${capitalizeWords(eventName)}** prayer will start in **5 minutes**.`
+                    break;
+            }
+
+            await this.sendMessage(payload, message)
+        })
+
+        sholatkuClient.on(PrayerEvent.PrayerIn15m, async (payload) => {
+            let eventName = payload.event.eventName
+            const eventTime = payload.event.time.format("HH:mm")
+            const dayName = moment().tz("Asia/Jakarta").locale("en").format("dddd")?.toLowerCase()
+            
+            let message: string | null = null
+
+            switch (eventName) {
+                case "imsak":
+                    message = `**Imsyakiyah** will begin in **15 minutes** at ${eventTime}.`
+                    break;
+
+                case "terbit":
+                    message = `**The sun will rise in 15 minutes** at ${eventTime}.`
+                    break;
+
+                case "dhuha":
+                    message = `**Dhuha** prayer will start in 15 minutes at ${eventTime}.`
+                    break;
+
+                default:
+                    if (dayName == "friday" && eventName == "dzuhur") {
+                        eventName = "jummah"
+                    }
+
+                    message = `**${capitalizeWords(eventName)}** prayer will start in **15 minutes**.`
+                    break;
+            }
+
+            await this.sendMessage(payload, message)
+        })
+
+        sholatkuClient.on(PrayerEvent.PrayerIn30m, async (payload) => {
+            const eventName = payload.event.eventName
+            const eventTime = payload.event.time.format("HH:mm")
+            // const dayName = moment().tz("Asia/Jakarta").locale("en").format("dddd")?.toLowerCase()
+            
+            let message: string | null = null
+
+            switch (eventName) {
+                case "imsak":
+                    message = `**Imsyakiyah** will begin in **30 minutes** at ${eventTime}.`
+                    break;
+
+                case "maghrib":
+                    message = `**${capitalizeWords(eventName)}** prayer will start in 30 minutes at ${eventTime}.`
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (!message) return
+
+            await this.sendMessage(payload, message)
+        })
+    }
+}
